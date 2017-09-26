3db31b24adb47ecea6eb15d5179127c0538ece50
isaccchoi <isaccchoi@naver.com>
2017-09-26 15:08:08 +0900

    add published, hide method to Post Model

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
-# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100)
@@ -12,3 +12,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def hide(self):
+        self.published_date = None
+        self.save()
