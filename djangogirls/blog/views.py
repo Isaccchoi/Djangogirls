033fdc09c2a105b53649fba6690f938b639c8d46
isaccchoi <isaccchoi@naver.com>
2017-09-30 16:26:30 +0900

    add pagination to post_list html

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.core import paginator
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -9,9 +11,19 @@ User = get_user_model()
 
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        post_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        post_list = paginator.page(paginator.num_pages)
     context = {
         # posts key의 value는 Queryset
-        'posts': posts
+        'post_list': post_list,
     }
     return render(request, 'blog/post_list.html', context)
 
@@ -27,36 +39,27 @@ def post_detail(request, pk):
 
 def post_add(request):
     # request 메소드가 POST이며 title 및 content가 잘 들어 있을 경우
-    if request.method == 'POST' and request.POST.get('title') and request.POST.get('content'):
-        title = request.POST['title']
-        content = request.POST['content']
-        author = User.objects.get(username='admin')
-        post = Post.objects.create(
-            title=title,
-            content=content,
-            author=author,
-        )
-        # publish에 체크를 할 경우 자동으로 현재 시간에 맞춰 published_date를 넣고 한번 더 저장
-        if request.POST.get('publish'):
-            post.publish()
-        return redirect('post_detail', pk=post.pk)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if title and content:
+            author = User.objects.get(username='admin')
+            post = Post.objects.create(
+                title=title,
+                content=content,
+                author=author,
+            )
+            # publish에 체크를 할 경우 자동으로 현재 시간에 맞춰 published_date를 넣고 한번 더 저장
+            if request.POST.get('publish'):
+                post.publish()
+            return redirect('post_detail', pk=post.pk)
+        # FIXME
     # request 메소드가 GET일 경우 입력할 수 있는 form 제공
-    elif request.method == 'GET':
-        context = {
-
-        }
-        return render(request, 'blog/post_form.html', context)
-    # request 메소드가 POST이면서 title 및 content가 '안'들어 있을 경우 alert값에 True를 넣어 알람이 나오도록 설정
-    elif request.method == 'POST' and not request.POST.get('title') or not request.POST.get('content'):
-        context = {
-            'alert': True
-        }
-        return render(request, 'blog/post_form.html', context)
     else:
         context = {
 
         }
-        return render(request, 'blog/post_form.html', context)
+    return render(request, 'blog/post_form.html', context)
 
 
 def post_delete(request, pk):
